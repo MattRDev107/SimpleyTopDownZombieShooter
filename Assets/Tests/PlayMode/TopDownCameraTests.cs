5c798cd870c524c9be52d7dd1980fe4a38c7bdb6
Matthew R <mattsdev107@gmail.com>
2021-03-30 06:37:22 -0500

    Create TopDownCamera branch

---
+++ Assets/Tests/PlayMode/TopDownCameraTests.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace TopDownShooter.Tests.PlayMode
+{
+	public class TopDownCameraTests
+	{
+		public class Resposition
+		{
+			[UnityTest]
+			public IEnumerator Offset_Is_Ten() 
+			{
+				yield return null;
+			}
+		}
+	}
+}
